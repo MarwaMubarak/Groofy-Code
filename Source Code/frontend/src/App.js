284c2171f6ef05336fe8fc965a6f75f97a595037
MahmoudAbdelrady <mahmoudten55@gmail.com>
2023-10-17 22:17:55 +0300

    Added React files

---
+++ Source Code/frontend/src/App.js
@@ -0,0 +1,18 @@
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Home from "./pages/Home/home";
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" />
+        <Route path="/clan" />
+        <Route path="/login" />
+        <Route path="/signup" />
+        <Route path="/news" />
+        <Route path="/help" />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+export default App;
