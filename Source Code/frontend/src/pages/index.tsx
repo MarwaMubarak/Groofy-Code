3ecef5a951ab0c8d4783868d9ce18ae293897323
MahmoudAbdelrady <112890203+MahmoudAbdelrady@users.noreply.github.com>
2023-10-27 17:54:03 +0200

    Added clan page coding design

--- Source Code/frontend/src/pages/index.tsx
+++ Source Code/frontend/src/pages/index.tsx
@@ -2,5 +2,6 @@ import Home from "./Home/home";
 import Match from "./Match/Match";
 import Login from "./Auth/Login";
 import SignUp from "./Auth/SignUp";
+import Clan from "./Clan/Clan";
 
-export { Home, Match, Login, SignUp };
+export { Home, Match, Login, SignUp, Clan };
