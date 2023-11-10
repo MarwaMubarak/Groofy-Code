4212a74a70cb2642645c8b1b4e9bb1efeb57a68a
MahmoudAbdelrady <112890203+MahmoudAbdelrady@users.noreply.github.com>
2023-11-10 22:35:25 +0200

    Initialized new design of homepage

--- Source Code/frontend/src/pages/New_Home/New_Home.tsx
+++ Source Code/frontend/src/pages/New_Home/New_Home.tsx
@@ -1,11 +1,34 @@
-import { GroofyHeader } from "../../components";
+import { GBtn, GroofyHeader } from "../../components";
 import "./scss/new_home.css";
 
 const New_Home = () => {
   return (
     <>
       <GroofyHeader idx={0} />
-      <div className="align"></div>
+      <div className="home-container">
+        <div className="play-container">
+          <div className="gamemode">
+            <div className="gm-info">
+              <h3>Casual</h3>
+              <p>
+                Empower players to create their perfect match by allowing them
+                to customize every aspect.
+              </p>
+            </div>
+            <GBtn btnText="Battle" clickEvent={() => {}} />
+          </div>
+          <div className="gamemode">
+            <div className="gm-info">
+              <h3>Ranked</h3>
+              <p>
+                Challenge your skills and climb the ranks with the option to
+                play in a competitive ranked match.
+              </p>
+            </div>
+            <GBtn btnText="Battle" clickEvent={() => {}} />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
