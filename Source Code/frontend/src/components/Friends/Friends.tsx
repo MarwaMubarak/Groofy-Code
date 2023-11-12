0889a90f83eb190607ff872107c9a15f30d68b93
Hazem Adel <74595174+hazemadelkhalel@users.noreply.github.com>
2023-11-12 20:44:38 +0200

    fix bugs of friends pop-up

--- Source Code/frontend/src/components/Friends/Friends.tsx
+++ Source Code/frontend/src/components/Friends/Friends.tsx
@@ -2,53 +2,6 @@ import { useState } from "react";
 import SingleFriend from "./SingleFriend/SingleFriend";
 import "./scss/friends.css";
 const Friends = () => {
-  // Assuming you have a list of friend elements with class "friend"
-  const friends: NodeListOf<Element> = document.querySelectorAll(
-    ".singlefriend-container"
-  );
-
-  friends.forEach((friend: Element) => {
-    friend.addEventListener("mouseenter", () => {
-      // Calculate the position of the friend relative to the viewport
-      const rect: DOMRect = friend.getBoundingClientRect();
-      // Adjust the position of the floating menu based on the friend's position
-      const floatingMenu: HTMLElement | null =
-        friend.querySelector(".sf-popup");
-      const menuHeight: number = floatingMenu ? floatingMenu.offsetHeight : 0;
-      // Check if the friend is near the bottom of the viewport
-      if (rect.bottom - 20 < menuHeight) {
-        if (floatingMenu) {
-          // floatingMenu.style.top = "auto";
-          floatingMenu.style.bottom = "0"; // Adjust as needed
-          console.log(window.innerHeight);
-          console.log(rect.bottom);
-          console.log(menuHeight);
-          console.log("First");
-        }
-      } else {
-        if (floatingMenu) {
-          floatingMenu.style.top = "100%"; // Adjust as needed
-          floatingMenu.style.bottom = "auto";
-          console.log(window.innerHeight);
-          console.log(rect.bottom);
-          console.log(menuHeight);
-          console.log("Second");
-        }
-      }
-      // Show the floating menu
-      if (floatingMenu) {
-        floatingMenu.style.display = "block";
-      }
-    });
-    friend.addEventListener("mouseleave", () => {
-      // Hide the floating menu on mouse leave
-      const floatingMenu: HTMLElement | null =
-        friend.querySelector(".sf-popup");
-      if (floatingMenu) {
-        floatingMenu.style.display = "none";
-      }
-    });
-  });
   const [friendsActive, setFriendsActive] = useState(false);
   return (
     <div className={`friends-container ${friendsActive}`}>
@@ -81,6 +34,17 @@ const Friends = () => {
           <SingleFriend />
           <SingleFriend />
           <SingleFriend />
+          <SingleFriend />
+          <SingleFriend />
+          <SingleFriend />
+          <SingleFriend />
+          <SingleFriend />
+          <SingleFriend />
+          <SingleFriend />
+          <SingleFriend />
+          <SingleFriend />
+
+
         </div>
       </div>
     </div>
