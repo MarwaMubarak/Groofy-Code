e18f997b95c6a38237a5669c2b9b5429c49a5685
Hazem Adel <74595174+hazemadelkhalel@users.noreply.github.com>
2024-01-30 03:33:51 +0200

    Fix importing clan controller bug

--- Source Code/backend/routes/clanRoute.js
+++ Source Code/backend/routes/clanRoute.js
@@ -1,13 +1,16 @@
 const router = require("express").Router();
-const calnController = require("../controllers/ClanController/ClanController");
-const { verifyToken } = require('../middleware/verifyToken');
+const calnController = require("../controllers/ClanController/clanController");
+const { verifyToken } = require("../middleware/verifyToken");
 
 //create Clan
-router.route('/clans/create').post(verifyToken, calnController.createClan);
+router.route("/clans/create").post(verifyToken, calnController.createClan);
 router.get("/clans", calnController.getAllClans);
-router.get('/clans/:clanId', calnController.getClanById);
-router.route('/clans/update/:clanId').put(verifyToken, calnController.updateClanById);
-router.route('/clans/delete/:clanId').delete(verifyToken, calnController.deleteClanById);
-
+router.get("/clans/:clanId", calnController.getClanById);
+router
+  .route("/clans/update/:clanId")
+  .put(verifyToken, calnController.updateClanById);
+router
+  .route("/clans/delete/:clanId")
+  .delete(verifyToken, calnController.deleteClanById);
 
 module.exports = router;
