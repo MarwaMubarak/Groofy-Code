fb1fd8fe2cfb8337d67af8b4bb3f157aeacb6741
Marwa Mubarak <marwamubarak6@gmail.com>
2024-01-31 16:55:41 +0200

    Update userModel.js

--- Source Code/backend/models/userModel.js
+++ Source Code/backend/models/userModel.js
@@ -3,129 +3,122 @@ const Joi = require("joi");
 const JoiPassComplex = require("joi-password-complexity");
 const Jwt = require("jsonwebtoken");
 
-const UserSchema = new mongoose.Schema(
-  {
+const UserSchema = new mongoose.Schema({
     username: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
-      minlength: 1,
-      maxlength: 100,
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 1,
+        maxlength: 100,
     },
     email: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
-      minlength: 4,
-      maxlength: 256,
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 4,
+        maxlength: 256,
     },
     password: {
-      type: String,
-      required: true,
-      trim: true,
-      minlength: 8,
-      maxlength: 256,
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 8,
+        maxlength: 256,
     },
     firstname: {
-      type: String,
-      trim: true,
-      minlength: 1,
-      maxlength: 256,
+        type: String,
+        trim: true,
+        minlength: 1,
+        maxlength: 256,
     },
     lastname: {
-      type: String,
-      trim: true,
-      minlength: 1,
-      maxlength: 256,
+        type: String,
+        trim: true,
+        minlength: 1,
+        maxlength: 256,
     },
     country: {
-      type: String,
-      trim: true,
-      minlength: 1,
-      maxlength: 100,
+        type: String,
+        trim: true,
+        minlength: 1,
+        maxlength: 100,
     },
     city: {
-      type: String,
-      trim: true,
-      minlength: 1,
-      maxlength: 100,
+        type: String,
+        trim: true,
+        minlength: 1,
+        maxlength: 100,
     },
     bio: {
-      type: String,
-      trim: true,
-      minlength: 0,
-      maxlength: 1000,
+        type: String,
+        trim: true,
+        minlength: 0,
+        maxlength: 1000,
     },
-    badges: [
-      {
+    badges: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: "Badge",
-      },
-    ],
+    }, ],
 
-    selectedBadges: [
-      {
+    selectedBadges: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: "Badge",
-      },
-    ],
+    }, ],
 
     photo: {
-      type: Object,
-      default: {
-        url: "https://images.are.na/eyJidWNrZXQiOiJhcmVuYV9pbWFnZXMiLCJrZXkiOiI4MDQwOTc0L29yaWdpbmFsX2ZmNGYxZjQzZDdiNzJjYzMxZDJlYjViMDgyN2ZmMWFjLnBuZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MTIwMCwiaGVpZ2h0IjoxMjAwLCJmaXQiOiJpbnNpZGUiLCJ3aXRob3V0RW5sYXJnZW1lbnQiOnRydWV9LCJ3ZWJwIjp7InF1YWxpdHkiOjkwfSwianBlZyI6eyJxdWFsaXR5Ijo5MH0sInJvdGF0ZSI6bnVsbH19?bc=0",
-        publicID: null,
-      },
+        type: Object,
+        default: {
+            url: "https://images.are.na/eyJidWNrZXQiOiJhcmVuYV9pbWFnZXMiLCJrZXkiOiI4MDQwOTc0L29yaWdpbmFsX2ZmNGYxZjQzZDdiNzJjYzMxZDJlYjViMDgyN2ZmMWFjLnBuZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MTIwMCwiaGVpZ2h0IjoxMjAwLCJmaXQiOiJpbnNpZGUiLCJ3aXRob3V0RW5sYXJnZW1lbnQiOnRydWV9LCJ3ZWJwIjp7InF1YWxpdHkiOjkwfSwianBlZyI6eyJxdWFsaXR5Ijo5MH0sInJvdGF0ZSI6bnVsbH19?bc=0",
+            publicID: null,
+        },
     },
     status: {
-      type: Boolean,
-      default: false,
+        type: Boolean,
+        default: false,
     },
     isOnline: {
-      type: Boolean,
-      default: false,
+        type: Boolean,
+        default: false,
     },
     totalMatch: {
-      type: Number,
-      default: 0,
+        type: Number,
+        default: 0,
     },
     Trophies: {
-      type: Number,
-      default: 0,
+        type: Number,
+        default: 0,
     },
     wins: {
-      type: Number,
-      default: 0,
+        type: Number,
+        default: 0,
     },
     loses: {
-      type: Number,
-      default: 0,
+        type: Number,
+        default: 0,
     },
     draws: {
-      type: Number,
-      default: 0,
+        type: Number,
+        default: 0,
     },
     division: {
-      type: String, // will change
-      default: "",
+        type: String, // will change
+        default: "",
     },
-  },
-  {
+}, {
     timestamps: true,
     toJSON: {
-      virtuals: true,
-      transform: function (doc, ret) {
-        delete ret.id;
-        delete ret.__v;
+        virtuals: true,
+        transform: function(doc, ret) {
+            delete ret.id;
+            delete ret.__v;
 
-        return ret;
-      },
+            return ret;
+        },
     },
     toObject: { virtuals: true },
-  }
-);
+});
 
 /*******************
  * Joining Tables
@@ -134,35 +127,35 @@ const UserSchema = new mongoose.Schema(
 
 // Comment Table
 UserSchema.virtual("comments", {
-  ref: "Comment",
-  localField: "_id",
-  foreignField: "user",
+    ref: "Comment",
+    localField: "_id",
+    foreignField: "user",
 });
 // Match Table
 UserSchema.virtual("matches", {
-  ref: "Match",
-  localField: "_id",
-  foreignField: { $in: ["team1", "team2"] },
+    ref: "Match",
+    localField: "_id",
+    foreignField: { $in: ["team1", "team2"] },
 });
 // Submission Table
 UserSchema.virtual("submissions", {
-  ref: "Submission",
-  localField: "_id",
-  foreignField: { $in: ["users"] },
+    ref: "Submission",
+    localField: "_id",
+    foreignField: { $in: ["users"] },
 });
 // Clan Table
 ////TO DO uncomment when possiable
 UserSchema.virtual("clan", {
-  ref: "Clan",
-  localField: "_id",
-  foreignField: { $in: ["members"] },
+    ref: "Clan",
+    localField: "_id",
+    foreignField: { $in: ["members"] },
 });
 
 // Generating token
-UserSchema.methods.generateAuthToken = function () {
-  return Jwt.sign({ id: this._id }, process.env.JWT_SECRETKEY, {
-    expiresIn: "30d",
-  });
+UserSchema.methods.generateAuthToken = function() {
+    return Jwt.sign({ id: this._id }, process.env.JWT_SECRETKEY, {
+        expiresIn: "30d",
+    });
 };
 
 // User model
@@ -174,58 +167,57 @@ const User = mongoose.model("User", UserSchema);
 
 // Sign Up
 const validateSignUp = (user) => {
-  const schema = Joi.object({
-    username: Joi.string().trim().min(1).max(100).required(),
-    email: Joi.string().trim().min(4).max(256).required().email(),
-    password: JoiPassComplex().required(),
-    country: Joi.string().trim().min(1).max(100),
-    firstname: Joi.string().trim().min(1).max(256),
-    lastname: Joi.string().trim().min(1).max(256),
-  });
-  return schema.validate(user);
+    const schema = Joi.object({
+        username: Joi.string().trim().min(1).max(100).required(),
+        email: Joi.string().trim().min(4).max(256).required().email(),
+        password: JoiPassComplex().required(),
+        country: Joi.string().trim().min(1).max(100),
+        firstname: Joi.string().trim().min(1).max(256),
+        lastname: Joi.string().trim().min(1).max(256),
+    });
+    return schema.validate(user);
 };
 
 // Login
 const validateLoginEmail = (user) => {
-  const schema = Joi.object({
-    email: Joi.string().trim().min(4).max(256).required().email(),
-    password: Joi.string().required(),
-  });
-  return schema.validate(user);
+    const schema = Joi.object({
+        email: Joi.string().trim().min(4).max(256).required().email(),
+        password: Joi.string().required(),
+    });
+    return schema.validate(user);
 };
 
 const validateLoginUserName = (user) => {
     const schema = Joi.object({
-      email: Joi.string().trim().min(4).max(100).required(),
-      password: Joi.string().required(),
+        email: Joi.string().trim().min(4).max(100).required(),
+        password: Joi.string().required(),
     });
     return schema.validate(user);
-  };
+};
 //Update User
 const validateUpdateUser = (user) => {
-  const badgeIds = (user.badges || []).map((badge) => badge._id.toString());
-  const schema = Joi.object({
-    firstname: Joi.string().trim().min(1).max(256),
-    lastname: Joi.string().trim().min(1).max(256),
-    country: Joi.string().trim().min(1).max(100),
-    city: Joi.string().trim().min(1).max(100),
-    bio: Joi.string().trim().max(1000),
-    friends: Joi.array().items(Joi.string()), // Assuming friends is an array of strings
-  });
-  return schema.validate(user);
+    const schema = Joi.object({
+        firstname: Joi.string().trim().max(256),
+        lastname: Joi.string().trim().max(256),
+        country: Joi.string().trim().max(100),
+        city: Joi.string().trim().max(100),
+        bio: Joi.string().trim().max(1000),
+        friends: Joi.array().items(Joi.string()), // Assuming friends is an array of strings
+    });
+    return schema.validate(user);
 };
 const validateChangePassword = (user) => {
-  const schema = Joi.object({
-    password: JoiPassComplex(),
-  });
-  return schema.validate(user);
+    const schema = Joi.object({
+        password: JoiPassComplex(),
+    });
+    return schema.validate(user);
 };
 
 module.exports = {
-  User,
-  validateSignUp,
-  validateLoginEmail,
-  validateLoginUserName,
-  validateUpdateUser,
-  validateChangePassword,
+    User,
+    validateSignUp,
+    validateLoginEmail,
+    validateLoginUserName,
+    validateUpdateUser,
+    validateChangePassword,
 };
