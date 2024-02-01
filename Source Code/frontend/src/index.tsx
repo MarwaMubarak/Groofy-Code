d9d5bfb7e28c03e721931ec3dc1cbb88a8654767
MahmoudAbdelrady <112890203+MahmoudAbdelrady@users.noreply.github.com>
2024-02-01 20:33:45 +0200

    Added a new search page

--- Source Code/frontend/src/index.tsx
+++ Source Code/frontend/src/index.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.module.css";
 import { Provider } from "react-redux";
 import store from "./store";
+import { PrimeReactProvider } from "primereact/api";
+import "./index.module.css";
 import "primereact/resources/themes/lara-dark-blue/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
-import { PrimeReactProvider } from "primereact/api";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
