0c90560e97b46226d530df5535b47c5200866a74
Marwa Mubarak <marwamubarak6@gmail.com>
2024-02-26 22:15:37 +0200

    spring boot - backend

---
+++ Source Code/new backend/GroofyCode/GroofyCode/src/main/java/com/groofycode/GroofyCode/GroofyCodeApplication.java
@@ -0,0 +1,13 @@
+package com.groofycode.GroofyCode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroofyCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroofyCodeApplication.class, args);
+	}
+
+}
