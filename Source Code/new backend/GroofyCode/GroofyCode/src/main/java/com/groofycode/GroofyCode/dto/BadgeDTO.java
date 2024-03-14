7c40bf9769d44d2a80b04fad05fc4de7808055df
Marwa Mubarak <marwamubarak6@gmail.com>
2024-03-14 08:06:15 +0200

    Badge Endpoints done

--- Source Code/new backend/GroofyCode/GroofyCode/src/main/java/com/groofycode/GroofyCode/dto/BadgeDTO.java
+++ Source Code/new backend/GroofyCode/GroofyCode/src/main/java/com/groofycode/GroofyCode/dto/BadgeDTO.java
@@ -5,6 +5,11 @@ import jakarta.validation.constraints.*;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.antlr.v4.runtime.misc.Pair;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 @Getter
@@ -22,7 +27,7 @@ public class BadgeDTO {
     private String photo;
 
     @NotBlank(message = "Description is required")
-    @Size(min = 4, max = 100, message =  " Description length must be between 4 and 100")
+    @Size(min = 4, max = 100, message =  "Description length must be between 4 and 100")
     private String description;
 
 
@@ -33,4 +38,33 @@ public class BadgeDTO {
         this.photo = photo;
         this.description = description;
     }
+
+
+//    public Pair<HashMap<String,List<String>>,BadgeDTO> updateBadge(BadgeDTO badgeDTO){
+//
+//        List<String> errors = new ArrayList<>();
+//
+//        if(badgeDTO.getDescription().length()<=100 && badgeDTO.getDescription().length()>=4){
+//            this.description = badgeDTO.getDescription();
+//        }else {
+//            errors.add( "Description length must be between 4 and 100");
+//        }
+//
+//        if(badgeDTO.getPhoto().length()>=4){
+//            this.photo = badgeDTO.getPhoto();
+//        }
+//
+//
+//        if(badgeDTO.getPhoto().length()<=100 && badgeDTO.getName().length()>=4){
+//            this.name = badgeDTO.getName();
+//        }else{
+//            errors.add( "Name length must be between 4 and 100");
+//
+//        }
+//
+//        HashMap errMap = new HashMap<>();
+//        errMap.put("errors",errors);
+//
+//        return (errMap, this)
+//    }
 }
