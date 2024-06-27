a1cf1df2c653c457facae3138a7e04737972e206
Hazem Adel <74595174+hazemadelkhalel@users.noreply.github.com>
2024-06-27 04:34:45 +0300

    Added Matchmaking styling

--- Source Code/frontend/src/App.tsx
+++ Source Code/frontend/src/App.tsx
@@ -14,18 +14,36 @@ import {
 } from "./pages";
 
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import TestingSocket from "./pages/TestingSocket/TestingSocket";
 import { WebSocketConnection } from "./components";
 import { userThunks, gameThunks } from "./store/actions";
 import toast, { Toaster } from "react-hot-toast";
 import classes from "./toast.module.css";
+const messages = [
+  "Searching for the perfect match...",
+  "Looking for a worthy opponent...",
+  "Scanning for challengers...",
+  "Finding your next rival...",
+  "Opponent search in progress...",
+  "Hunting for a competitor...",
+  "Matching you with a formidable foe...",
+  "Connecting you to a challenger...",
+  "Opponent hunt underway...",
+  "Seeking a worthy adversary...",
+];
+
+const getRandomMessage = () =>
+  messages[Math.floor(Math.random() * messages.length)];
 
 function App() {
   const loggedUser = useSelector((state: any) => state.auth.user);
   const toastShow = useSelector((state: any) => state.toast.toastShow);
   const inQueue = useSelector((state: any) => state.game.inQueue);
   const dispatch = useDispatch();
+
+  const [message, setMessage] = useState(getRandomMessage());
+  const [toastId, setToastId] = useState<string | null>(null);
 
   useEffect(() => {
     const getProfile = async () => {
@@ -40,34 +58,77 @@ function App() {
   console.log("QUEUE", inQueue);
 
   useEffect(() => {
-    const getToast = () =>
-      toast.custom(
-        (t) => (
-          <div className={classes.toast_testing}>
-            <div className={classes.top}>
-              <i className="pi pi-spin pi-spinner"></i>
-              <span>Finding opponent...</span>
-            </div>
-            <div
-              className={classes.cancel_btn}
-              onClick={() => {
-                dispatch(gameThunks.leaveQueue() as any);
-                toast.dismiss(t.id);
-              }}
-            >
-              Cancel
-            </div>
-          </div>
-        ),
-        {
-          duration: Infinity,
-          position: "bottom-right",
-        }
-      );
+    const interval = setInterval(() => {
+      setMessage(getRandomMessage());
+    }, 5000); // Change message every 5 seconds
+
     if (toastShow || inQueue) {
-      getToast();
+      if (!toastId) {
+        const id = toast.custom(
+          (t) => (
+            <div className={classes.toast_testing}>
+              <div className={classes.top}>
+                <i className="pi pi-spin pi-spinner"></i>
+                <span>{message}</span>
+              </div>
+              <div
+                className={classes.cancel_btn}
+                onClick={() => {
+                  dispatch(gameThunks.leaveQueue() as any);
+                  toast.dismiss(t.id);
+                  setToastId(null);
+                }}
+              >
+                Cancel
+              </div>
+            </div>
+          ),
+          {
+            duration: Infinity,
+            position: "bottom-right",
+          }
+        );
+        setToastId(id);
+      } else {
+        toast.custom(
+          (t) => (
+            <div className={classes.toast_testing}>
+              <div className={classes.toast_header}>
+                <h3>QUEUEING</h3>
+              </div>
+              <div className={classes.top}>
+                <div className={classes.top_title}>
+                  <i className="pi pi-spin pi-spinner"></i>
+                  <h1>Matchmaking</h1>
+                </div>
+                <span>{message}</span>
+              </div>
+              <div
+                className={classes.cancel_btn}
+                onClick={() => {
+                  dispatch(gameThunks.leaveQueue() as any);
+                  toast.dismiss(t.id);
+                  setToastId(null);
+                }}
+              >
+                CANCEL
+              </div>
+            </div>
+          ),
+          {
+            id: toastId, // Ensure it updates the existing toast
+            duration: Infinity,
+            position: "bottom-right",
+          }
+        );
+      }
+    } else if (toastId) {
+      toast.dismiss(toastId);
+      setToastId(null);
     }
-  }, [dispatch, inQueue, toastShow]);
+
+    return () => clearInterval(interval); // Cleanup on unmount
+  }, [dispatch, inQueue, message, toastShow, toastId]);
 
   if (!loggedUser && localStorage.getItem("token")) {
     return <div>Loading...</div>;
